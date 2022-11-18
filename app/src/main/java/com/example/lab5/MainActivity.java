57c3d44a47e48f1dc3756b3f832d674d81d3067f
Manuel <a20190133@pucp.edu.pe>
2022-11-18 08:22:57 -0500

    Commit prueba inisesion

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
     }
+
+    public boolean verificarEstadoInternet() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public void iniciarSesion(View view){
+        if (verificarEstadoInternet()) {
+            Intent intent = new Intent(this, InicioSesion.class);
+            startActivity(intent);
+        } else {
+            MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(MainActivity.this);
+            builder.setMessage("Verifique su conexión a internet para poder ingresar a la aplicación");
+            builder.setPositiveButton("Aceptar",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+            builder.show();
+        }
+
+    }
+
+    public void registroUsuario(View view){
+        if (verificarEstadoInternet()) {
+            Intent intent = new Intent(this,RegistrarUsuario.class);
+            startActivity(intent);
+        } else {
+            MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(MainActivity.this);
+            builder.setMessage("Verifique su conexión a internet para poder ingresar a la aplicación");
+            builder.setPositiveButton("Aceptar",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+            builder.show();
+        }
+    }
+
 }
