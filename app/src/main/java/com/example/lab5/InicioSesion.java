6fb4861a3576624fbe65c0d198c95958c42585b3
Manuel <a20190133@pucp.edu.pe>
2022-11-18 08:50:48 -0500

    Registro, inicio de sesion, cambio password implementado

--- app/src/main/java/com/example/lab5/InicioSesion.java
+++ app/src/main/java/com/example/lab5/InicioSesion.java
@@ -1,10 +1,19 @@
 package com.example.lab5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class InicioSesion extends AppCompatActivity {
 
@@ -19,8 +28,67 @@ public class InicioSesion extends AppCompatActivity {
 
         String mensaje_exito = getIntent().getStringExtra("exito");
         if (mensaje_exito != null && !mensaje_exito.equals("")) {
-            Snackbar.make(findViewById(R.id.activity_iniciar_sesion), mensaje_exito, Snackbar.LENGTH_LONG).show();
+            Snackbar.make(findViewById(R.id.activity_inicio_sesion), mensaje_exito, Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    public void cambiarContrasenia(View view){
+        Intent intent = new Intent(this, CambioContrasenia.class);
+        startActivity(intent);
+    }
+
+    public void validarInicioSesion(View view) {
+        TextInputLayout correo = findViewById(R.id.inputCorreo_iniSesion);
+        TextInputLayout password = findViewById(R.id.inputPassword_iniSesion);
+
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z.]+";
+        boolean correoValido = true;
+        if (correo.getEditText().getText().toString() != null && !correo.getEditText().getText().toString().equals("")) {
+            if (!correo.getEditText().getText().toString().matches(emailPattern)) {
+                correo.setError("Ingrese un correo válido");
+                correoValido = false;
+            } else {
+                correo.setErrorEnabled(false);
+            }
+        } else {
+            correo.setError("Ingrese un correo");
+            correoValido = false;
         }
 
+        boolean passwordValido = true;
+        if (password.getEditText().getText().toString() != null && !password.getEditText().getText().toString().equals("")) {
+            password.setErrorEnabled(false);
+        } else {
+            password.setError("Ingrese una contraseña");
+            passwordValido = false;
+        }
+
+        if (correoValido && passwordValido) {
+            firebaseAuth.signInWithEmailAndPassword(correo.getEditText().getText().toString(), password.getEditText().getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Log.d("task", "EXITO EN REGISTRO");
+
+                        firebaseAuth.getCurrentUser().reload().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (firebaseAuth.getCurrentUser().isEmailVerified()) {
+                                    //Intent intent = new Intent(InicioSesion.this, .class);
+                                    //startActivity(intent);
+                                } else {
+                                    Snackbar.make(findViewById(R.id.activity_inicio_sesion), "Su cuenta no ha sido verificada. Verifíquela para poder ingresar", Snackbar.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+                    } else {
+                        Log.d("task", "ERROR EN REGISTRO - " + task.getException().getMessage());
+                        Snackbar.make(findViewById(R.id.activity_inicio_sesion), task.getException().getMessage(), Snackbar.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+        }
     }
 }
--- app/src/main/java/com/example/lab5/RegistrarUsuario.java
+++ app/src/main/java/com/example/lab5/RegistrarUsuario.java
@@ -1,14 +1,106 @@
 package com.example.lab5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrarUsuario extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+
+    boolean codigoValido = true;
+    boolean passwordValido = true;
+    boolean verifyPasswordValido = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_usuario);
+        getSupportActionBar().setTitle("Registro de usuario");
+
+        firebaseAuth = firebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+    }
+
+    public void validarRegistro(View view) {
+        TextInputLayout correo = findViewById(R.id.inputCorreo_registro);
+        TextInputLayout password = findViewById(R.id.inputPassword_registro);
+        TextInputLayout verifyPassword = findViewById(R.id.inputVerifyPassword_registro);
+
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z.]+";
+        boolean correoValido = true;
+        if (correo.getEditText().getText().toString() != null && !correo.getEditText().getText().toString().equals("")) {
+            if (!correo.getEditText().getText().toString().matches(emailPattern)) {
+                correo.setError("Ingrese un correo válido");
+                correoValido = false;
+            } else {
+                correo.setErrorEnabled(false);
+            }
+        } else {
+            correo.setError("Ingrese un correo");
+            correoValido = false;
+        }
+
+        boolean passwordValido = true;
+        if (password.getEditText().getText().toString() != null && !password.getEditText().getText().toString().equals("")) {
+            password.setErrorEnabled(false);
+        } else {
+            password.setError("Ingrese una contraseña");
+            passwordValido = false;
+        }
+
+        if (verifyPassword.getEditText().getText().toString() != null && !verifyPassword.getEditText().getText().toString().equals("")) {
+
+            verifyPassword.setErrorEnabled(false);
+        } else {
+            verifyPassword.setError("Debe verificar su contraseña");
+            verifyPasswordValido = false;
+        }
+
+        if (codigoValido && correoValido && passwordValido && verifyPasswordValido) {
+            Log.d("task", "Registro valido");
+
+            firebaseAuth.createUserWithEmailAndPassword(correo.getEditText().getText().toString(), password.getEditText().getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Log.d("task", "EXITO EN REGISTRO");
+
+                        firebaseAuth.getCurrentUser().sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Log.d("task", "EXITO EN ENVIO DE CORREO DE VERIFICACION");
+                                Intent intent = new Intent(RegistrarUsuario.this, InicioSesion.class);
+                                intent.putExtra("exito", "Se ha enviado un correo para la verificación de su cuenta");
+                                startActivity(intent);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("task", "ERROR EN ENVIO DE CORREO DE VERIFICACION - " + e.getMessage());
+                            }
+                        });
+
+                    } else {
+                        Log.d("task", "ERROR EN REGISTRO - " + task.getException().getMessage());
+                    }
+                }
+            });
+        }
     }
 }
